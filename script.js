3ec409fd5aadc497dbe8a963a620af50abdd6e0a
MasonMcKayla <iameyl@gmail.com>
2026-07-13 15:41:04 -0400

    Add functions to manage page visibility and login

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
+function hideAll(){
+
+document.getElementById("homePage").classList.add("hidden");
+document.getElementById("loginPage").classList.add("hidden");
+document.getElementById("producerPage").classList.add("hidden");
+
+}
+
+function goHome(){
+
+hideAll();
+
+document.getElementById("homePage").classList.remove("hidden");
+
+}
+
+function showLogin(){
+
+hideAll();
+
+document.getElementById("loginPage").classList.remove("hidden");
+
+}
+
+function login(){
+
+const pw=document.getElementById("password").value;
+
+if(pw==="010305"){
+
+hideAll();
+
+document.getElementById("producerPage").classList.remove("hidden");
+
+}
+else{
+
+alert("Wrong password.");
+
+}
+
+}
+
+function startSession(){
+
+alert("Recording page will be built next.");
+
+}
 
