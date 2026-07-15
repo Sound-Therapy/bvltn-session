1d4e9f6279849b84ef524e9142d2558685a0ee6e
MasonMcKayla <iameyl@gmail.com>
2026-07-15 14:44:37 -0400

    Fix formatting of session ID display in script.js

--- script.js
+++ script.js
@@ -109,7 +109,7 @@ async function showSessionManager() {
 
                 <h3>${session.session_name}</h3>
 
-                <p><b>ID:</b> ${JSON.stringify(session)}</p>
+                <b>ID:</b> ${JSON.stringify(session
                 <p><b>Code:</b> ${session.session_token}</p>
                 <button class="openBtn">
                     Open
