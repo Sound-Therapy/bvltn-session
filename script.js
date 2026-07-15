fc572f14cdf63789c8e5681de9aa0613c57c030e
MasonMcKayla <iameyl@gmail.com>
2026-07-15 12:09:38 -0400

    Change session ID display to JSON string format

--- script.js
+++ script.js
@@ -106,7 +106,7 @@ async function showSessionManager() {
 
                 <h3>${session.session_name}</h3>
 
-                <p><b>ID:</b> ${session.id}</p>
+                <p><b>ID:</b> ${JSON.stringify(session)}</p>
                 <p><b>Code:</b> ${session.session_token}</p>
                 <button class="openBtn">
                     Open
